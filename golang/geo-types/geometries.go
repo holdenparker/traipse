ca14308409908f8d0bda86d70f1aedb645d0901a
holdenparker <holdenparker55@gmail.com>
2025-06-22 15:08:22 -0400

    chore: Abstracting Marshal and Unmarshal logic.

--- golang/geo-types/geometries.go
+++ golang/geo-types/geometries.go
@@ -2,6 +2,8 @@ package geotypes
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"reflect"
 )
 
@@ -15,8 +17,39 @@ const (
 	GeometryCollectionType = "GeometryCollection"
 )
 
+var UnmarshallingTypeMismatch = errors.New("Unmarshal type field mismatch")
+
 type geometryBuilder[CoordinateShape any] struct {
 	Coordinates CoordinateShape `json:"coordinates"`
+}
+
+func GeoJSONMarshalFactory[coordsType any](typeString string, coords coordsType) ([]byte, error) {
+	// This type "rename" prevents circular unmarshalling
+	type GeomType geometryBuilder[coordsType]
+	return json.Marshal(struct {
+		GeomType
+		Type string `json:"type"`
+	}{
+		Type:     PointType,
+		GeomType: GeomType{Coordinates: coords},
+	})
+}
+
+func GeoJSONUnmarshalFactory[coordsType any](typeString string, dflt coordsType, data []byte) (coordsType, error) {
+	type GeomType geometryBuilder[coordsType]
+	result := &struct {
+		GeomType
+		Type string `json:"type"`
+	}{}
+	err := json.Unmarshal(data, result)
+	if err != nil {
+		return dflt, err
+	}
+	if result.Type != typeString {
+		return dflt, fmt.Errorf("%w: Expected %v, found %v", UnmarshallingTypeMismatch, typeString, result.Type)
+	}
+
+	return result.Coordinates, nil
 }
 
 type GeoJSON interface {
--- golang/geo-types/point.go
+++ golang/geo-types/point.go
@@ -1,7 +1,6 @@
 package geotypes
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 )
@@ -9,25 +8,14 @@ import (
 type PointCoords = Position
 type PointGeometry geometryBuilder[PointCoords]
 
-var (
-	UnmarshallingPointTypeMismatch  = errors.New("Expecting to unmarshal to Point geometry type")
-	PointGeometryUnmarshallingError = errors.New("PointGeometry UnmarshalJSON error")
-)
+var PointGeometryUnmarshallingError = errors.New("PointGeometry UnmarshalJSON error")
 
 func (p PointGeometry) IsValid() bool {
 	return true
 }
 
 func (p PointGeometry) MarshalJSON() ([]byte, error) {
-	// This type "rename" prevents circular unmarshalling
-	type Pg PointGeometry
-	return json.Marshal(struct {
-		Pg
-		Type string `json:"type"`
-	}{
-		Type: PointType,
-		Pg:   Pg{Coordinates: p.Coordinates},
-	})
+	return GeoJSONMarshalFactory(PointType, p.Coordinates)
 }
 
 func (p PointGeometry) Type() string {
@@ -35,21 +23,12 @@ func (p PointGeometry) Type() string {
 }
 
 func (p *PointGeometry) UnmarshalJSON(data []byte) error {
-	// This type "rename" prevents circular unmarshalling
-	type Pg PointGeometry
-	result := &struct {
-		Pg
-		Type string `json:"type"`
-	}{}
-	err := json.Unmarshal(data, result)
+	coords, err := GeoJSONUnmarshalFactory(PointType, PointCoords{}, data)
+
 	if err != nil {
 		return fmt.Errorf("%w: %w", PointGeometryUnmarshallingError, err)
 	}
-	if result.Type != PointType {
-		return fmt.Errorf("%w: %w, found %s", PointGeometryUnmarshallingError, UnmarshallingPointTypeMismatch, result.Type)
-	}
-
-	p.Coordinates = result.Coordinates
+	p.Coordinates = coords
 
 	return nil
 }
